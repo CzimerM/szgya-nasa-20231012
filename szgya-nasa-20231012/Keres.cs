1f577c2c8bb6a53cbad9922d15b19ae57f5e16dc
Airamek <vegkeppnemairamek@gmail.com>
2023-10-12 09:30:44 +0200

    Add project files.

---
+++ szgya-nasa-20231012/Keres.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szgya_nasa_20231012
+{
+    class Keres
+    {
+        public string Cim { get; set; }
+        public DateTime KeresIdeje { get; set; }
+        public string Lekerdezes { get; set; }
+        public int AllapotKod { get; set; }
+        public int ValaszMeret { get; set; }
+
+        public bool Domain { get {
+                string[] cimKomp = this.Cim.Split('.');
+                int temp;
+                bool isNum = int.TryParse(cimKomp.Last(), out temp);
+                return !isNum;
+            }
+        }
+
+        public Keres(string sor)
+        {
+            string[] adatok = sor.Split('*');
+            this.Cim = adatok[0];
+            //this.KeresIdeje = DateTime.Parse(adatok[1]);
+            string[] datum1 = adatok[1].Split('/');
+            string[] datum2 = datum1.Last().Split(':');
+            this.Lekerdezes = adatok[2];
+            this.KeresIdeje = DateTime.Parse($"{datum2[0]}/{datum1[1]}/{datum1[0]} {datum2[1]}:{datum2[2]}:{datum2[3]}");
+            string[] meta = adatok[3].Split(' ');
+            this.AllapotKod = int.Parse(meta[0]);
+            this.ValaszMeret = meta[1][0] == '-' ? 0 : int.Parse(meta[1]);
+        }
+    }
+}
---
+++ szgya-nasa-20231012/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+namespace szgya_nasa_20231012
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Keres> keresek = new List<Keres>();
+
+            using StreamReader sr = new StreamReader("../../../src/NASAlog.txt", Encoding.UTF8);
+            while(!sr.EndOfStream)
+            {
+                keresek.Add(new Keres(sr.ReadLine()));
+            }
+
+            Console.WriteLine($"5.feladat: Kérések száma: {keresek.Count}");
+
+            Console.WriteLine($"6.feladat: Válaszok összes mérete: {keresek.Sum(k => k.ValaszMeret)} byte");
+
+            Console.WriteLine($"8.feladat: Domain-es kérések: {(float)keresek.Count(k => k.Domain) / (float) keresek.Count * 100:0.00}%");
+
+            Console.WriteLine($"9.feladat: Statisztika:");
+            var f9stat = keresek
+                .GroupBy(k => k.AllapotKod)
+                .ToDictionary(k => k.Key, v => v.Count());
+
+            foreach (var kod in f9stat)
+            {
+                Console.WriteLine($"\t{kod.Key}: {kod.Value} db");
+            }
+
+        }
+    }
+}
